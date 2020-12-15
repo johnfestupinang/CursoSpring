2c98fc4fa74f0668c837fe3708db3691778997b8
johnfestupinang <47452365+johnfestupinang@users.noreply.github.com>
2020-12-14 19:40:52 -0500

    Seccion13, se agrega el codigo de la seccion 13 spring security

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -2,10 +2,14 @@ package com.bolsadeideas.springboot.app.controllers;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collection;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -13,6 +17,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -40,7 +52,10 @@ public class ClienteController {
 
 	@Autowired
 	private IUploadFileService uploadFileService;
+	
+	protected final Log logger = LogFactory.getLog(this.getClass());
 
+	@Secured({"ROLE_USER","ROLE_ADMIN"})
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
 
@@ -58,6 +73,7 @@ public class ClienteController {
 				.body(recurso);
 	}
 
+	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN','ROLE_OTRO')")
 	@GetMapping(value = "/ver/{id}")
 	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 
@@ -73,9 +89,40 @@ public class ClienteController {
 		return "ver";
 	}
 
-	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+	@RequestMapping(value = {"/listar", "/"}, method = RequestMethod.GET)
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model, Authentication authentication,
+			HttpServletRequest request) {
 
+		if(authentication != null) {
+			logger.info("Hola usuario autenticado, tu username es: ".concat(authentication.getName()));
+		}
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		if(auth != null) {
+			logger.info("UTLIZANDO AUTH de forma estática Hola usuario autenticado, tu username es: ".concat(auth.getName()));
+		}
+		
+		
+		if(hasRole("ROLE_ADMIN")) {
+			logger.info("Hola ".concat(auth.getName().concat(" tienes acceso")));
+		}else {
+			logger.info("Hola ".concat(auth.getName().concat(" NO tienes acceso")));
+		}
+		
+		SecurityContextHolderAwareRequestWrapper securityContext = new SecurityContextHolderAwareRequestWrapper(request, "ROLE_");
+		if(securityContext.isUserInRole("ADMIN")) {
+			logger.info("Forma, utilizando SecurityContextHolderAwareRequestWrapper:  ".concat(auth.getName().concat(" tienes acceso")));
+		}else {
+			logger.info("Forma, utilizando SecurityContextHolderAwareRequestWrapper: ".concat(auth.getName().concat(" NO tienes acceso")));
+		}
+		
+		if(request.isUserInRole("ROLE_ADMIN")) {
+			logger.info("Forma, utilizando HttpServletRequest:  ".concat(auth.getName().concat(" tienes acceso")));
+		}else {
+			logger.info("Forma, utilizando HttpServletRequest: ".concat(auth.getName().concat(" NO tienes acceso")));
+		}
+		
 		Pageable pageRequest = PageRequest.of(page, 4);
 
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
@@ -87,6 +134,7 @@ public class ClienteController {
 		return "listar";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form")
 	public String crear(Map<String, Object> model) {
 
@@ -96,6 +144,7 @@ public class ClienteController {
 		return "form";
 	}
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@RequestMapping(value = "/form/{id}")
 	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 
@@ -116,6 +165,7 @@ public class ClienteController {
 		return "form";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
 			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
@@ -154,6 +204,7 @@ public class ClienteController {
 		return "redirect:listar";
 	}
 
+	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/eliminar/{id}")
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 
@@ -169,5 +220,30 @@ public class ClienteController {
 
 		}
 		return "redirect:/listar";
+	}
+	
+	private boolean hasRole(String role) {
+		SecurityContext context = SecurityContextHolder.getContext();
+		if(context == null) {
+			return false;
+		}
+		
+		Authentication auth = context.getAuthentication();
+		
+		if(auth == null) {
+			return false;
+		}
+		
+		//Collection<?extends GrantedAuthority> ?extends -> quiere decir que admite cualquier objeto que herede de la interfaz GrantedAuthority
+		Collection<?extends GrantedAuthority> authorities = auth.getAuthorities();
+		return authorities.contains(new SimpleGrantedAuthority(role));
+		
+//		for(GrantedAuthority authority: authorities) {
+//			if(role.equals(authority.getAuthority())){
+//				return true;
+//			}
+//		}
+//		
+//		return false;
 	}
 }
