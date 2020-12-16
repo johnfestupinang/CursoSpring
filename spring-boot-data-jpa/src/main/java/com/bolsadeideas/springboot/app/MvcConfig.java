b051fa7a8655f4574df9f5dd2c4252f2bfd10b41
johnfestupinang <47452365+johnfestupinang@users.noreply.github.com>
2020-12-15 20:11:47 -0500

    Se agrega codigo d ela seccion 16, y se adjunta ejemplo del docente del curso

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.app;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 
 // import java.nio.file.Paths;
@@ -9,9 +11,13 @@ import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -31,6 +37,27 @@ public class MvcConfig implements WebMvcConfigurer {
 		*/
 //	}
 	
+	//Resuelve el locale y en donde se va a guardar en este caso en la sesion 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		localResolver.setDefaultLocale(new Locale("es","ES"));
+		return localResolver;
+	}
+	
+	//Interceptor, permite cambiar el lenguaje
+	@Bean
+	public LocaleChangeInterceptor localChangeInterceptor() {
+		LocaleChangeInterceptor localInterceptor = new LocaleChangeInterceptor();
+		localInterceptor.setParamName("lang");
+		return localInterceptor;
+	}
+	
+	//Registrando el interceptor
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localChangeInterceptor());
+	}
 
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/error_403").setViewName("error_403");
--- spring-boot-data-jpa_EjemploDocente/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa_EjemploDocente/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -14,6 +14,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.core.io.Resource;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -48,6 +49,8 @@ import com.bolsadeideas.springboot.app.util.paginator.PageRender;
 @Controller
 @SessionAttributes("cliente")
 public class ClienteController {
+	
+	protected final Log logger = LogFactory.getLog(this.getClass());
 
 	@Autowired
 	private IClienteService clienteService;
@@ -57,10 +60,8 @@ public class ClienteController {
 	
 	@Autowired
 	private MessageSource messageSource;
-	
-	protected final Log logger = LogFactory.getLog(this.getClass());
 
-	@Secured({"ROLE_USER","ROLE_ADMIN"})
+	@Secured({"ROLE_USER"})
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
 
@@ -69,7 +70,6 @@ public class ClienteController {
 		try {
 			recurso = uploadFileService.load(filename);
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -78,62 +78,63 @@ public class ClienteController {
 				.body(recurso);
 	}
 
-	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN','ROLE_OTRO')")
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping(value = "/ver/{id}")
-	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash, Locale locale) {
 
 		Cliente cliente = clienteService.fetchByIdWithFacturas(id);
-				//clienteService.findOne(id);
 		if (cliente == null) {
-			flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+			flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.db.error", null, locale));
 			return "redirect:/listar";
 		}
 
 		model.put("cliente", cliente);
-		model.put("titulo", "Detalle cliente: " + cliente.getNombre());
+		model.put("titulo", messageSource.getMessage("text.cliente.detalle.titulo", null, locale).concat(": ").concat(cliente.getNombre()));
 		return "ver";
 	}
 
 	@RequestMapping(value = {"/listar", "/"}, method = RequestMethod.GET)
-	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model, Authentication authentication,
-			HttpServletRequest request,Locale locale) {
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model,
+			Authentication authentication,
+			HttpServletRequest request,
+			Locale locale) {
 
 		if(authentication != null) {
 			logger.info("Hola usuario autenticado, tu username es: ".concat(authentication.getName()));
 		}
-		
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		
+
 		if(auth != null) {
-			logger.info("UTLIZANDO AUTH de forma estática Hola usuario autenticado, tu username es: ".concat(auth.getName()));
+			logger.info("Utilizando forma estática SecurityContextHolder.getContext().getAuthentication(): Usuario autenticado: ".concat(auth.getName()));
 		}
-		
 		
 		if(hasRole("ROLE_ADMIN")) {
-			logger.info("Hola ".concat(auth.getName().concat(" tienes acceso")));
-		}else {
-			logger.info("Hola ".concat(auth.getName().concat(" NO tienes acceso")));
+			logger.info("Hola ".concat(auth.getName()).concat(" tienes acceso!"));
+		} else {
+			logger.info("Hola ".concat(auth.getName()).concat(" NO tienes acceso!"));
 		}
 		
-		SecurityContextHolderAwareRequestWrapper securityContext = new SecurityContextHolderAwareRequestWrapper(request, "ROLE_");
-		if(securityContext.isUserInRole("ADMIN")) {
-			logger.info("Forma, utilizando SecurityContextHolderAwareRequestWrapper:  ".concat(auth.getName().concat(" tienes acceso")));
-		}else {
-			logger.info("Forma, utilizando SecurityContextHolderAwareRequestWrapper: ".concat(auth.getName().concat(" NO tienes acceso")));
-		}
+		SecurityContextHolderAwareRequestWrapper securityContext = new SecurityContextHolderAwareRequestWrapper(request, "");
 		
+		if(securityContext.isUserInRole("ROLE_ADMIN")) {
+			logger.info("Forma usando SecurityContextHolderAwareRequestWrapper: Hola ".concat(auth.getName()).concat(" tienes acceso!"));
+		} else {
+			logger.info("Forma usando SecurityContextHolderAwareRequestWrapper: Hola ".concat(auth.getName()).concat(" NO tienes acceso!"));
+		}
+
 		if(request.isUserInRole("ROLE_ADMIN")) {
-			logger.info("Forma, utilizando HttpServletRequest:  ".concat(auth.getName().concat(" tienes acceso")));
-		}else {
-			logger.info("Forma, utilizando HttpServletRequest: ".concat(auth.getName().concat(" NO tienes acceso")));
-		}
+			logger.info("Forma usando HttpServletRequest: Hola ".concat(auth.getName()).concat(" tienes acceso!"));
+		} else {
+			logger.info("Forma usando HttpServletRequest: Hola ".concat(auth.getName()).concat(" NO tienes acceso!"));
+		}	
 		
 		Pageable pageRequest = PageRequest.of(page, 4);
 
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
 
 		PageRender<Cliente> pageRender = new PageRender<Cliente>("/listar", clientes);
-		model.addAttribute("titulo", messageSource.getMessage("text.cliente.listar.titulo",  null, locale));
+		model.addAttribute("titulo", messageSource.getMessage("text.cliente.listar.titulo", null, locale));
 		model.addAttribute("clientes", clientes);
 		model.addAttribute("page", pageRender);
 		return "listar";
@@ -141,42 +142,42 @@ public class ClienteController {
 
 	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form")
-	public String crear(Map<String, Object> model) {
+	public String crear(Map<String, Object> model, Locale locale) {
 
 		Cliente cliente = new Cliente();
 		model.put("cliente", cliente);
-		model.put("titulo", "Formulario de Cliente");
+		model.put("titulo", messageSource.getMessage("text.cliente.form.titulo.crear", null, locale));
 		return "form";
 	}
 
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@RequestMapping(value = "/form/{id}")
-	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash, Locale locale) {
 
 		Cliente cliente = null;
 
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
 			if (cliente == null) {
-				flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD!");
+				flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.db.error", null, locale));
 				return "redirect:/listar";
 			}
 		} else {
-			flash.addFlashAttribute("error", "El ID del cliente no puede ser cero!");
+			flash.addFlashAttribute("error", messageSource.getMessage("text.cliente.flash.id.error", null, locale));
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
-		model.put("titulo", "Editar Cliente");
+		model.put("titulo", messageSource.getMessage("text.cliente.form.titulo.editar", null, locale));
 		return "form";
 	}
 
 	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
 	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
-			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
+			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status, Locale locale) {
 
 		if (result.hasErrors()) {
-			model.addAttribute("titulo", "Formulario de Cliente");
+			model.addAttribute("titulo", messageSource.getMessage("text.cliente.form.titulo", null, locale));
 			return "form";
 		}
 
@@ -196,12 +197,13 @@ public class ClienteController {
 				e.printStackTrace();
 			}
 
-			flash.addFlashAttribute("info", "Has subido correctamente '" + uniqueFilename + "'");
+			flash.addFlashAttribute("info", messageSource.getMessage("text.cliente.flash.foto.subir.success", null, locale) + "'" + uniqueFilename + "'");
 
 			cliente.setFoto(uniqueFilename);
+
 		}
 
-		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito!" : "Cliente creado con éxito!";
+		String mensajeFlash = (cliente.getId() != null) ? messageSource.getMessage("text.cliente.flash.editar.success", null, locale) : messageSource.getMessage("text.cliente.flash.crear.success", null, locale);
 
 		clienteService.save(cliente);
 		status.setComplete();
@@ -211,16 +213,17 @@ public class ClienteController {
 
 	@Secured("ROLE_ADMIN")
 	@RequestMapping(value = "/eliminar/{id}")
-	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash, Locale locale) {
 
 		if (id > 0) {
 			Cliente cliente = clienteService.findOne(id);
 
 			clienteService.delete(id);
-			flash.addFlashAttribute("success", "Cliente eliminado con éxito!");
+			flash.addFlashAttribute("success", messageSource.getMessage("text.cliente.flash.eliminar.success", null, locale));
 
 			if (uploadFileService.delete(cliente.getFoto())) {
-				flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminada con exito!");
+				String mensajeFotoEliminar = String.format(messageSource.getMessage("text.cliente.flash.foto.eliminar.success", null, locale), cliente.getFoto());
+				flash.addFlashAttribute("info", mensajeFotoEliminar);
 			}
 
 		}
@@ -228,7 +231,9 @@ public class ClienteController {
 	}
 	
 	private boolean hasRole(String role) {
+		
 		SecurityContext context = SecurityContextHolder.getContext();
+		
 		if(context == null) {
 			return false;
 		}
@@ -239,16 +244,9 @@ public class ClienteController {
 			return false;
 		}
 		
-		//Collection<?extends GrantedAuthority> ?extends -> quiere decir que admite cualquier objeto que herede de la interfaz GrantedAuthority
-		Collection<?extends GrantedAuthority> authorities = auth.getAuthorities();
+		Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+		
 		return authorities.contains(new SimpleGrantedAuthority(role));
 		
-//		for(GrantedAuthority authority: authorities) {
-//			if(role.equals(authority.getAuthority())){
-//				return true;
-//			}
-//		}
-//		
-//		return false;
 	}
 }
