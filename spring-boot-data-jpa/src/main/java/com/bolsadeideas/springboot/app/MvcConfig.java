a16c4f1e6078ad6a1812e4ce41746c015d6acccb
johnfestupinang <47452365+johnfestupinang@users.noreply.github.com>
2020-12-16 20:25:58 -0500

    Seccion 20, Convertir documento a XML

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 //import org.slf4j.LoggerFactory;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -68,5 +69,11 @@ public class MvcConfig implements WebMvcConfigurer {
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Bean 
+	public Jaxb2Marshaller jaxb2Marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(new Class[] {com.bolsadeideas.springboot.app.view.xml.ClienteList.class});//Clases que se van a convertir a xml
+		return marshaller;
+	}
 
 }
